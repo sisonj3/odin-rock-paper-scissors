d77ece7ddabebf2f8e47d5d083f78a42935fade9
sisonj <98570174+sisonj3@users.noreply.github.com>
2023-09-22 13:21:17 -0700

    changed console logs to DOM methods

--- rps.js
+++ rps.js
@@ -1,3 +1,9 @@
+let score = 0;
+
+const choices = Array.from(document.querySelectorAll('.choice'));
+console.log(choices);
+choices.forEach(choice => choice.addEventListener('click', playRound));
+
 function getComputerChoice(){
     /* Get a random number between 0 and 2 */
     let choice = Math.floor(Math.random() * 3);
@@ -14,40 +20,43 @@ function getComputerChoice(){
 /* Plays a single round of rock, paper, scissors */
 function playRound(e, computerSelection = getComputerChoice()){
 
-    /* Set playerChoice based on button pressed */
-    let playerChoice = e.target.textContent;
-    console.log(playerChoice + " vs " + computerSelection);
-
     /* Win/Loss Strings */
     const win = "You Win! ";
     const lose = "You Lose! ";
 
+    //Query Selectors
+    const vs = document.querySelector('.vs');
+    const score = document.querySelector('.score');
+    const result = document.querySelector('.result');
+
+    /* Set playerChoice based on button pressed */
+    let playerChoice = e.target.textContent;
+    vs.textContent = playerChoice + " VS " + computerSelection;
+
     if (playerChoice == computerSelection){
-        return "Draw!";
+        result.textContent = "Draw!";
     } else if (playerChoice == "Rock") {
         if (computerSelection == "Scissors") {
-            return win + playerChoice + " beats " + computerSelection;
+            result.textContent = win + playerChoice + " beats " + computerSelection;
         } else {
-            return lose + computerSelection + " beats " + playerChoice;
+            result.textContent = lose + computerSelection + " beats " + playerChoice;
         }
     } else if (playerChoice == "Paper") {
         if (computerSelection == "Rock") {
-            return win + playerChoice + " beats " + computerSelection;
+            result.textContent = win + playerChoice + " beats " + computerSelection;
         } else {
-            return lose + computerSelection + " beats " + playerChoice;
+            result.textContent = lose + computerSelection + " beats " + playerChoice;
         }
     } else if (playerChoice == "Scissors") {
         if (computerSelection == "Paper") {
-            return win + playerChoice + " beats " + computerSelection;
+            result.textContent = win + playerChoice + " beats " + computerSelection;
         } else {
-            return lose + computerSelection + " beats " + playerChoice;
+            result.textContent = lose + computerSelection + " beats " + playerChoice;
         }
     } else {
-        return "Invalid player selection!";
+        result.textContent = "Invalid player selection!";
     }
 
 }
 
-const choices = Array.from(document.querySelectorAll('.choice'));
-console.log(choices);
-choices.forEach(choice => choice.addEventListener('click', playRound));
+
